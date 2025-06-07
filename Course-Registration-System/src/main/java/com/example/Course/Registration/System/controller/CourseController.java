a683e0b6f0b9f9d0579de77082f11852819f02f9
codexgokul <gokulofx@gmail.com>
2025-06-07 15:47:42 +0530

    init commit

---
+++ Course-Registration-System/src/main/java/com/example/Course/Registration/System/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.example.Course.Registration.System.controller;
+
+import com.example.Course.Registration.System.model.Course;
+import com.example.Course.Registration.System.model.CourseRegistry;
+import com.example.Course.Registration.System.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("courses")
+    public List<Course> availableCourses(){
+        return courseService.availableCourses();
+    }
+    @GetMapping("courses/enrolled")
+    public List<CourseRegistry>enrolledStudents(){
+        return courseService.enrolledStudents();
+    }
+
+    @PostMapping("/course/register")
+    public String enrollCourse(@RequestParam("name") String name,@RequestParam("emailId") String emailId,@RequestParam("courseName") String courseName ) {
+        courseService.enrollCourse(name,emailId,courseName);
+        return "Congratulations!"+name+"Enrollment Successful for"+courseName;
+    }
+}
